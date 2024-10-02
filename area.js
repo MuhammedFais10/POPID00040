04c58efc77c64f29abe9fd512dfc2c374788e3ec
Muhammed Fais <pmuhammedfais2001@gmail.com>
2024-10-02 13:10:54 -1000

    comit

---
+++ area.js
@@ -0,0 +1,5 @@
+function areaOfTriangle(a, b, c) {
+  const s = (a + b + c) / 2;
+  return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+}
+console.log(areaOfTriangle(5, 6, 7));
---
+++ largestElement.js
@@ -0,0 +1,4 @@
+function findLargest(arr) {
+  return Math.max(...arr);
+}
+console.log(findLargest([1, 2, 3, 4, 5])); // Output: 5
